a7604a36c53ca107110bc3ef4445caa85c6180d9
seokrae <kslbsh@gmail.com>
2022-11-27 16:45:54 +0900

    feat: 코틀린 플러그인 및 의존성 추가, EmailAddress: java to kotlin -> equals & hashCode도 코틀린 방식과 자바 방식이 있는거 같은데 무슨 차이일까?

--- src/main/java/com/example/travelator/travel/java/EmailAddress.java
+++ src/main/java/com/example/travelator/travel/java/EmailAddress.java
@@ -1,2 +1,50 @@
-package com.example.travelator.travel.java;public class EmailAddress {
+package com.example.travelator.travel.java;
+
+import java.util.Objects;
+
+/**
+ * EmailAddress Java implementation
+ */
+public class EmailAddress {
+	private final String localPart;
+	private final String domain;
+	
+	public EmailAddress(String localPart, String domain) {
+		this.localPart = localPart;
+		this.domain = domain;
+	}
+	
+	public static EmailAddress parse(String value) {
+		var atIndex = value.lastIndexOf('@');
+		if(atIndex < 1 || atIndex == value.length() - 1) {
+			throw new IllegalArgumentException("EmailAddress must be two parts separated by @");
+		}
+		return new EmailAddress(value.substring(0, atIndex), value.substring(atIndex + 1));
+	}
+	
+	public String getLocalPart() {
+		return localPart;
+	}
+	
+	public String getDomain() {
+		return domain;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof EmailAddress that)) return false;
+		return Objects.equals(localPart, that.localPart)
+			&& Objects.equals(domain, that.domain);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(localPart, domain);
+	}
+	
+	@Override
+	public String toString() {
+		return localPart + "@" + domain;
+	}
 }
