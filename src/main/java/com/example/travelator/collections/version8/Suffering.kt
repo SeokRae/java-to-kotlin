d87b419bb7e1904b2bc9524f49a4e55cc5ea4ab2
seokrae <kslbsh@gmail.com>
2022-12-07 17:43:21 +0900

    feat: 6.3.2 최종 리펙토링

--- src/main/java/com/example/travelator/collections/version8/Suffering.kt
+++ src/main/java/com/example/travelator/collections/version8/Suffering.kt
@@ -4,12 +4,11 @@ import com.example.travelator.collections.version7.Other.SOME_COMPLICATED_RESULT
 
 object Suffering {
     @JvmStatic
-    fun sufferScoreFor(route: List<Journey>): Int {
-        return sufferScore(
+    fun sufferScoreFor(route: List<Journey>): Int =
+        sufferScore(
             route.longestJourneys(limit = 3),
             Routes.getDepartsFrom(route)
         )
-    }
 
     // longestJourneys가 파라미터를 변경하지 않았기 때문에 함수를 단일식 함수로 변경
     @JvmStatic
@@ -19,9 +18,8 @@ object Suffering {
     /**
      * 데이터를 제 자리에서 변경하는 코드 -> 새로운 값을 계산하고 참조가 이 새 값을 가리키게 하는 코드로 변경
      */
-    fun routesToShowFor(itineraryId: String?): List<List<Journey>> {
-        return bearable(Other.routesFor(itineraryId))
-    }
+    fun routesToShowFor(itineraryId: String?): List<List<Journey>> =
+        bearable(Other.routesFor(itineraryId))
 
     private fun bearable(routes: List<List<Journey>>): List<List<Journey>> =
         routes.filter { sufferScoreFor(it) <= 10 }
@@ -29,7 +27,6 @@ object Suffering {
     private fun sufferScore(
         longestJourneys: List<Journey>,
         start: Location,
-    ): Int {
-        return SOME_COMPLICATED_RESULT()
-    }
+    ): Int =
+        SOME_COMPLICATED_RESULT()
 }
