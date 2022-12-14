ecb1f66152fc4d7a21975bc0c54d69b9c7193f4b
seokrae <kslbsh@gmail.com>
2022-12-14 17:25:15 +0900

    feat: handle 메서드에서 시간 관련 값을 생성하고 있기 때문에 동작(action)

--- src/main/java/com/example/travelator/actiontocalculation/version3/CurrentTripsHandler.java
+++ src/main/java/com/example/travelator/actiontocalculation/version3/CurrentTripsHandler.java
@@ -2,6 +2,8 @@ package com.example.travelator.actiontocalculation.version3;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.time.Instant;
+
 import static java.net.HttpURLConnection.*;
 
 public class CurrentTripsHandler {
@@ -12,13 +14,13 @@ public class CurrentTripsHandler {
 		this.tracking = tracking;
 	}
 	
-	public Response handler(Request request) {
+	public Response handle(Request request) {
 		try {
 			var customerId = request.getQueryParam("customerId").stream().findFirst();
 			if (customerId.isEmpty()) {
 				return new Response(HTTP_BAD_REQUEST);
 			}
-			var currentTrip = tracking.currentTripFor(customerId.get(), null);
+			var currentTrip = tracking.currentTripFor(customerId.get(), Instant.now());
 			
 			return currentTrip.isPresent() ?
 				new Response(HTTP_OK, objectMapper.writeValueAsString(currentTrip))
