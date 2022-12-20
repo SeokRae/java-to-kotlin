940df44c54d352dcb6d35a630c467b218053c1d2
seokrae <kslbsh@gmail.com>
2022-12-21 01:15:31 +0900

    feat: 다중식 함수에서 단일식 함수로 -> p.172 9.21 처럼 안됨 어디다가 대고 수신객체 변경 호출을 해야 할지?

---
+++ src/main/java/com/example/travelator/_9multiexpressiontosingleexpression/version15/EmailAddress.kt
@@ -0,0 +1,31 @@
+package com.example.travelator._9multiexpressiontosingleexpression.version15
+
+data class EmailAddress(
+    val localPart: String,
+    val domain: String,
+) {
+    override fun toString(): String = "$localPart@$domain"
+
+    companion object {
+        @JvmStatic
+        fun parse(value: String): EmailAddress {
+            val (leftPart, rightPart) = value.split("@")
+            // 로컬 변수로 분리 후 메서드로 추출
+            return EmailAddress(
+                leftPart,
+                rightPart
+            )
+        }
+
+        // 추출된 메서드는 Pair를 반환되도록 처리됨
+        private fun String.split(divider: Char): Pair<String, String> { // convert parameter receiver
+            val atIndex = lastIndexOf(divider)
+            require(!(atIndex < 1 || atIndex == length - 1)) {
+                "EmailAddress must be two parts seperated by @"
+            }
+            val leftPart = substring(0, atIndex)
+            val rightPart = substring(atIndex + 1)
+            return Pair(leftPart, rightPart)
+        }
+    }
+}
