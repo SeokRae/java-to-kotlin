b1322018958b41ddcdba5cebfe48dff12db6b685
seokrae <kslbsh@gmail.com>
2022-12-21 00:26:18 +0900

    feat: 다중식 함수에서 단일식 함수로 -> p.168 9.13 let으로 둘러싸보기

---
+++ src/main/java/com/example/travelator/_9multiexpressiontosingleexpression/version9/EmailAddress.kt
@@ -0,0 +1,24 @@
+package com.example.travelator._9multiexpressiontosingleexpression.version9
+
+data class EmailAddress(
+    val localPart: String,
+    val domain: String,
+) {
+    override fun toString(): String = "$localPart@$domain"
+
+    companion object {
+        @JvmStatic
+        fun parse(value: String): EmailAddress {
+            val atIndex = value.lastIndexOf("@")
+            atIndex.let {
+                require(!(atIndex < 1 || atIndex == value.length - 1)) {
+                    "Invalid email address: $value"
+                }
+                return EmailAddress(
+                    value.substring(0, atIndex),
+                    value.substring(atIndex + 1)
+                )
+            }
+        }
+    }
+}
