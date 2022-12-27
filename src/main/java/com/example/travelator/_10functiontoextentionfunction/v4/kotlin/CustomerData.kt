92e2a7354bdd762b9237cea4da4c28b1450c23db
seokrae <kslbsh@gmail.com>
2022-12-27 23:49:09 +0900

    feat: 10. 함수를 확장 함수로 -> 10.11 식으로 변환 및 문자열 연결 템플릿으로 변환

---
+++ src/main/java/com/example/travelator/_10functiontoextentionfunction/v4/kotlin/CustomerData.kt
@@ -0,0 +1,9 @@
+package com.example.travelator._10functiontoextentionfunction.v4.kotlin
+
+data class CustomerData(
+    val id: String,
+    val givenName: String,
+    val familyName: String,
+    val score: Int,
+    val spend: Double,
+)
