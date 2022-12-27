939d64a46f44477738a607467a087f580acc331e
seokrae <kslbsh@gmail.com>
2022-12-28 00:44:57 +0900

    feat: 10. 함수를 확장 함수로 -> 10.13 formattedAs 는 어디서 관습적인 코드라고 함

---
+++ src/main/java/com/example/travelator/_10functiontoextentionfunction/v6/kotlin/HighValueCustomersReport.kt
@@ -0,0 +1,67 @@
+package com.example.travelator._10functiontoextentionfunction.v6.kotlin
+
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.Reader
+import java.io.Writer
+import java.util.*
+
+object HighValueCustomersReport {
+    @JvmStatic
+    @Throws(IOException::class)
+    fun generate(reader: Reader?, writer: Writer) {
+        val valuableCustomers = BufferedReader(reader).lines()
+            .skip(1)
+            .map { line: String -> customerDataFrom(line) }
+            .filter { (_, _, _, score): CustomerData -> score >= 10 }
+            .sorted(Comparator.comparing { (_, _, _, score) -> score })
+            .toList()
+        writer.append("ID\tName\tSpend\n")
+        for (customerData in valuableCustomers) {
+            writer.append(lineFor(customerData)).append("\n")
+        }
+        writer.append(summaryFor(valuableCustomers))
+    }
+
+    @JvmStatic
+    private fun customerDataFrom(line: String): CustomerData {
+        val parts = line.split("\t".toRegex()).toTypedArray()
+        val spend: Double = if (parts.size == 4) 0.0 else parts[4].toDouble()
+        return CustomerData(
+            parts[0],
+            parts[1],
+            parts[2],
+            parts[3].toInt(),
+            spend
+        )
+    }
+
+    private fun summaryFor(valuableCustomers: List<CustomerData>): String {
+        val total = valuableCustomers.stream()
+            .mapToDouble({ (_, _, _, _, spend) -> spend })
+            .sum()
+        return "\tTOTAL\t" + total.toMoneyString()
+    }
+
+    /**
+     * 1. convert function to property
+     * 2. convert expression to body
+     */
+    private fun lineFor(customerData: CustomerData): String =
+        "${customerData.id}\t${customerData.marketingName}\t${customerData.spend.toMoneyString()}"
+
+    /**
+     * 1. convert parameter to receiver
+     * 2. convert expression to body
+     */
+    private fun Double.toMoneyString(): String = this.formattedAs("%#.2f")
+
+    private fun Double.formattedAs(format: String) = String.format(format, this)
+
+    /**
+     * 1. convert function to property
+     * 2. convert expression to body
+     */
+    private val CustomerData.marketingName: String
+        get() = "${familyName.uppercase()}, $givenName"
+}
