a95cf04380b7a72e303f10ec0dccbd4fd224eb9e
seokrae <kslbsh@gmail.com>
2022-12-29 23:54:12 +0900

    feat: 11. 메서드에서 프로퍼티로 -> fullName 계산된 프로퍼티

---
+++ src/main/java/com/example/travelator/_11methodtoproperty/v3/java/PersonWithPublicFields.java
@@ -0,0 +1,33 @@
+package com.example.travelator._11methodtoproperty.v3.java;
+
+import java.time.LocalDate;
+
+public class PersonWithPublicFields {
+	
+	private final String givenName;
+	private final String familyName;
+	private final LocalDate dateOfBirth;
+	
+	public PersonWithPublicFields(String givenName, String familyName, LocalDate dateOfBirth) {
+		this.givenName = givenName;
+		this.familyName = familyName;
+		this.dateOfBirth = dateOfBirth;
+	}
+	
+	public String getGivenName() {
+		return givenName;
+	}
+	
+	public String getFamilyName() {
+		return familyName;
+	}
+	
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+	
+	public String getFullName() {
+		return givenName + " " + familyName;
+	}
+	
+}
