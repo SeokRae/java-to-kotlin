aa51a0f4b2ddad8858a26c9b3249bb98bd8380cb
seokrae <kslbsh@gmail.com>
2023-01-01 02:48:05 +0900

    feat: 11. 메서드에서 프로퍼티로 -> 사용 범위가 넓다면 계산된 프로퍼티를 함수 뒤로 감춰서 최초 호출 시 발생하는 잠재적인 계산 비용에 대한 힌트를 제공

---
+++ src/main/java/com/example/travelator/_11methodtoproperty/v8/kotlin/PersonWithProperties.kt
@@ -0,0 +1,25 @@
+package com.example.travelator._11methodtoproperty.v8.kotlin
+
+import java.time.LocalDate
+import java.time.Period
+
+data class PersonWithProperties(
+    val givenName: String,
+    val familyName: String,
+    val dateOfBirth: LocalDate,
+) {
+    val fullName: String get() = "$givenName $familyName"
+
+    fun age() = Period.between(dateOfBirth, LocalDate.now()).years
+
+    fun computeHash(): ByteArray = sumSlowHashOf(givenName, familyName, dateOfBirth.toString())
+
+
+    private val hash: ByteArray by lazy { sumSlowHashOf(givenName, familyName, dateOfBirth.toString()) }
+
+    fun hash() = hash
+
+    private fun sumSlowHashOf(givenName: String, familyName: String, toString: String): ByteArray {
+        TODO("Not yet implemented")
+    }
+}
