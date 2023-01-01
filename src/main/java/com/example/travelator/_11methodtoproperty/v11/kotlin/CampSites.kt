b370c51a4f72268a6068bdc05a186353d1714ca5
seokrae <kslbsh@gmail.com>
2023-01-01 19:32:34 +0900

    feat: 11. 메서드에서 프로퍼티로 -> 단일 식으로 변경

--- src/main/java/com/example/travelator/_11methodtoproperty/v11/kotlin/CampSites.kt
+++ src/main/java/com/example/travelator/_11methodtoproperty/v11/kotlin/CampSites.kt
@@ -5,11 +5,9 @@ import java.util.stream.Collectors
 fun Set<CampSite>.sitesInRegion(
     countryISO: String,
     region: String?
-): Set<CampSite> {
-    return stream()
-        .filter { campSite: CampSite ->
-            campSite.countryCode == countryISO
-                    && campSite.region.equals(region, ignoreCase = true)
-        }
-        .collect(Collectors.toUnmodifiableSet())
-}
+): Set<CampSite> = stream()
+    .filter { campSite: CampSite ->
+        campSite.countryCode == countryISO
+                && campSite.region.equals(region, ignoreCase = true)
+    }
+    .collect(Collectors.toUnmodifiableSet())
