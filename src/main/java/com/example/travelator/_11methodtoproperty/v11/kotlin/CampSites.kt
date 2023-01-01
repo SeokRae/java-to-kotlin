9b320889c761f510cd03d211411c9510f28d092d
seokrae <kslbsh@gmail.com>
2023-01-01 19:44:01 +0900

    feat: 11. 메서드에서 프로퍼티로 -> 11.8 ~ 9 스트림에서 이터러블이나 시퀀스 이건 개념적인 부분은 모르지만 일단 따라간

--- src/main/java/com/example/travelator/_11methodtoproperty/v11/kotlin/CampSites.kt
+++ src/main/java/com/example/travelator/_11methodtoproperty/v11/kotlin/CampSites.kt
@@ -1,13 +1,10 @@
 package com.example.travelator._11methodtoproperty.v11.kotlin
 
-import java.util.stream.Collectors
-
-fun Set<CampSite>.sitesInRegion(
+fun Iterable<CampSite>.sitesInRegion(
     countryISO: String,
-    region: String?
-): Set<CampSite> = stream()
-    .filter { campSite: CampSite ->
-        campSite.countryCode == countryISO
-                && campSite.region.equals(region, ignoreCase = true)
-    }
-    .collect(Collectors.toUnmodifiableSet())
+    region: String,
+): Set<CampSite> =
+    filter { site ->
+        site.countryCode == countryISO
+                && site.region.equals(region, ignoreCase = true)
+    }.toSet()
