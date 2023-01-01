baf090fa84c60428d434b2201b7d43cff0d1fe3e
seokrae <kslbsh@gmail.com>
2023-01-01 19:32:02 +0900

    feat: 11. 메서드에서 프로퍼티로 -> 파라미터를 수신객체로 변환

--- src/main/java/com/example/travelator/_11methodtoproperty/v11/kotlin/CampSites.kt
+++ src/main/java/com/example/travelator/_11methodtoproperty/v11/kotlin/CampSites.kt
@@ -2,12 +2,11 @@ package com.example.travelator._11methodtoproperty.v11.kotlin
 
 import java.util.stream.Collectors
 
-fun sitesInRegion(
-    sites: Set<CampSite>,
+fun Set<CampSite>.sitesInRegion(
     countryISO: String,
     region: String?
 ): Set<CampSite> {
-    return sites.stream()
+    return stream()
         .filter { campSite: CampSite ->
             campSite.countryCode == countryISO
                     && campSite.region.equals(region, ignoreCase = true)
