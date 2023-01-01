50163755e4de8e050bc0682b87f86f2f8bc98601
seokrae <kslbsh@gmail.com>
2023-01-01 19:29:36 +0900

    feat: 11. 메서드에서 프로퍼티로 -> 코틀린으로 변경

--- src/main/java/com/example/travelator/_11methodtoproperty/v10/kotlin/CampSites.kt
+++ src/main/java/com/example/travelator/_11methodtoproperty/v10/kotlin/CampSites.kt
@@ -1,20 +1,18 @@
-package com.example.travelator._11methodtoproperty.v10.kotlin;
+package com.example.travelator._11methodtoproperty.v10.kotlin
 
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.stream.Collectors
 
-public class CampSites {
-	
-	public static Set<CampSite> sitesInRegion(
-		Set<CampSite> sites,
-		String countryISO,
-		String region
-	) {
-		return sites.stream()
-				.filter(campSite ->
-					campSite.getCountryCode().equals(countryISO)
-					&& campSite.getRegion().equalsIgnoreCase(region)
-				)
-				.collect(Collectors.toUnmodifiableSet());
-	}
+object CampSites {
+    fun sitesInRegion(
+        sites: Set<CampSite>,
+        countryISO: String,
+        region: String?
+    ): Set<CampSite> {
+        return sites.stream()
+            .filter { campSite: CampSite ->
+                campSite.countryCode == countryISO
+                        && campSite.region.equals(region, ignoreCase = true)
+            }
+            .collect(Collectors.toUnmodifiableSet())
+    }
 }
