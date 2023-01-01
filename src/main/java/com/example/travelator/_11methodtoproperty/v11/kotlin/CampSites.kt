507d1b2051054172b0bc11518addcee4c551c996
seokrae <kslbsh@gmail.com>
2023-01-01 19:54:03 +0900

    feat: 11. 메서드에서 프로퍼티로 -> 11.10 ~ 11 메서드 추출 이후 when 절로 변경

--- src/main/java/com/example/travelator/_11methodtoproperty/v11/kotlin/CampSites.kt
+++ src/main/java/com/example/travelator/_11methodtoproperty/v11/kotlin/CampSites.kt
@@ -1,10 +1,22 @@
 package com.example.travelator._11methodtoproperty.v11.kotlin
 
+/**
+ * 지역이 엔티티가 아니라 문자열일 뿐
+ * 왜냐면 country를 string으로 갖고 있다면 동일한 다른 지역의 이름이 겹칠 수 있기 때문
+ *
+ */
+
 fun Iterable<CampSite>.sitesInRegion(
     countryISO: String,
     region: String,
 ): Set<CampSite> =
-    filter { site ->
-        site.countryCode == countryISO
-                && site.region.equals(region, ignoreCase = true)
-    }.toSet()
+    filter { site -> site.isIn(countryISO, region) }.toSet()
+
+private fun CampSite.isIn(
+    countryISO: String,
+    region: String? = null,
+) = when(region) {
+    null -> countryCode == countryISO
+    else -> countryCode == countryISO
+            && this.region.equals(region, ignoreCase = true)
+}
