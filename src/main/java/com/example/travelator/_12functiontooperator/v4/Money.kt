bd1e19aa4ec0445fcdeadaa421bcb81746c67bb6
seokrae <kslbsh@gmail.com>
2023-01-03 00:10:23 +0900

    feat: 12. 함수에서 연산자로 -> 12.9 bigDecimal 연산자 + 로처리

---
+++ src/main/java/com/example/travelator/_12functiontooperator/v4/Money.kt
@@ -0,0 +1,41 @@
+package com.example.travelator._12functiontooperator.v4
+
+import java.math.BigDecimal
+import java.util.*
+
+class Money private constructor(
+    val amount: BigDecimal,
+    val currency: Currency,
+) {
+    override fun equals(other: Any?) =
+        this === other ||
+                other is Money &&
+                amount == other.amount &&
+                currency == other.currency
+
+    override fun hashCode() =
+        Objects.hash(amount, currency)
+
+    override fun toString() =
+        amount.toString() + " " + currency.currencyCode
+
+    /**
+     * replace with +
+     */
+    fun add(that: Money): Money = this + that
+
+    operator fun plus(that: Money): Money {
+        require(currency == that.currency) {
+            "cannot add Money values of different currencies"
+        }
+        return Money(this.amount + that.amount, currency)
+    }
+
+    companion object {
+        @JvmStatic
+        fun of(amount: BigDecimal, currency: Currency) = Money(
+            amount.setScale(currency.defaultFractionDigits),
+            currency
+        )
+    }
+}
