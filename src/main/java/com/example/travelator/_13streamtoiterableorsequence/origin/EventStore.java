b362dd29e6c97a97fa036ea708917c4172d4da17
seokrae <kslbsh@gmail.com>
2023-01-03 20:24:40 +0900

    feat: 13.스트림에서 이터러블이나 시퀀스로 -> origin 코드

---
+++ src/main/java/com/example/travelator/_13streamtoiterableorsequence/origin/EventStore.java
@@ -0,0 +1,16 @@
+package com.example.travelator._13streamtoiterableorsequence.origin;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public interface EventStore {
+
+    Iterator<Map<String, Object>> query(String query);
+
+    default Stream<Map<String, Object>> queryAsStream(String query) {
+        Iterable<Map<String, Object>> iterable = () -> query(query);
+        return StreamSupport.stream(iterable.spliterator(), false);
+    }
+}
