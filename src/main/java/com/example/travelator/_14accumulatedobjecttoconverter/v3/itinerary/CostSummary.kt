10bf8358b8cc6159adbfaa2db18c319f98c2fbcf
seokrae <kslbsh@gmail.com>
2023-01-17 00:41:39 +0900

    :pencil2: 14. 누적시키는 객체에서 변환으로 -> 14.14 불변 코드로 리펙토링

--- src/main/java/com/example/travelator/_14accumulatedobjecttoconverter/v3/itinerary/CostSummary.kt
+++ src/main/java/com/example/travelator/_14accumulatedobjecttoconverter/v3/itinerary/CostSummary.kt
@@ -9,22 +9,7 @@ class CostSummary(
     val lines: List<CurrencyConversion>,
 
 ) {
-
-    private val _lines = mutableListOf<CurrencyConversion>()
-
-    var total: Money = Money.of(0, userCurrency)
-        private set
-
-    // 끔찍한 코드 아닌가?
-    init {
-        lines.forEach {
-            total += it.toMoney
-        }
-    }
-
-    fun addLine(line: CurrencyConversion) {
-        _lines.add(line)
-        // add에서 plus로 변경하면 += 연산자를 사용할 수 있음
-        total += line.toMoney
-    }
+    var total = lines
+        .map { it.toMoney }
+        .fold(Money(0, userCurrency), Money::add)
 }
