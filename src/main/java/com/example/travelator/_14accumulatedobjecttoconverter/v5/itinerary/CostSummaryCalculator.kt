c51ca41867f9f7e30381ef53d05ff21ab319d259
seokrae <kslbsh@gmail.com>
2023-01-17 16:16:23 +0900

    :pencil2: 14. 누적시키는 객체에서 변환으로 -> 14.26 코드 리펙토링

--- src/main/java/com/example/travelator/_14accumulatedobjecttoconverter/v5/itinerary/CostSummaryCalculator.kt
+++ src/main/java/com/example/travelator/_14accumulatedobjecttoconverter/v5/itinerary/CostSummaryCalculator.kt
@@ -9,6 +9,7 @@ class CostSummaryCalculator(
     private val exchangeRates: ExchangeRates,
 ) {
     fun summarise(costs: Iterable<Money>): CostSummary {
+        // 인스턴스 필드를 제거하는 데는 성공 했으나 아직 가변 map을 사용하고 있는 문제가 있다.
         val currencyTotals = mutableMapOf<Currency, Money>()
         costs.forEach {
             currencyTotals.merge(it.currency, it, Money::plus)
